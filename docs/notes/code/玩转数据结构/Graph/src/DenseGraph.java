f17a67abea67626e06b05f920322bcbd296ce8a8
Biyu-GitHub <790125037@qq.com>
2019-08-18 20:16:32 +0800

    Update Code

---
+++ docs/notes/code/玩转数据结构/Graph/src/DenseGraph.java
@@ -0,0 +1,16 @@
+/**
+ * 稠密图--邻接矩阵
+ */
+public class DenseGraph {
+
+    int n; // 节点个数
+    int m; // 边的个数
+    boolean directed; // 是否是有向图
+    boolean[][] g; // 邻接矩阵
+
+    public DenseGraph(int n, boolean directed) {
+        this.n = n;
+        this.directed = directed;
+        g = new boolean[n][n];
+    }
+}
