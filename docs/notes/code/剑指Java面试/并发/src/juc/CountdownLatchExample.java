22ab6929777b3e67aa2664a321639228b07f6488
Yu Bi <790125037@qq.com>
2019-09-05 11:23:15 +0800

    Update Code

--- docs/notes/code/剑指Java面试/并发/src/juc/CountdownLatchExample.java
+++ docs/notes/code/剑指Java面试/并发/src/juc/CountdownLatchExample.java
@@ -7,7 +7,7 @@ import java.util.concurrent.Executors;
 public class CountdownLatchExample {
     public static void main(String[] args) throws InterruptedException {
         final int totalThread = 10;
-        CountDownLatch countDownLatch = new CountDownLatch(3);
+        CountDownLatch countDownLatch = new CountDownLatch(totalThread);
         ExecutorService executorService = Executors.newCachedThreadPool();
         for (int i = 0; i < totalThread; i++) {
             executorService.execute(() -> {
--- docs/notes/code/玩转数据结构/AVL/src/AVLTree.java
+++ docs/notes/code/玩转数据结构/AVL/src/AVLTree.java
@@ -70,10 +70,13 @@ public class AVLTree<K extends Comparable<K>, V> {
 
         node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
 
-        // 计算平衡因子 TODO
+        // 计算平衡因子
         int balanceFactor = getBalanceFactor(node);
-        if (Math.abs(balanceFactor) > 1)
-            System.out.println("unbalanced : " + balanceFactor);
+//        if (Math.abs(balanceFactor) > 1)
+//            System.out.println("unbalanced : " + balanceFactor);
+        // TODO
+        if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0)
+            ;
 
         return node;
     }
@@ -188,7 +191,7 @@ public class AVLTree<K extends Comparable<K>, V> {
         inorder(node.right, keys);
     }
 
-    // 判断该二叉树是否是一棵二分搜索树2
+    // 判断该二叉树是否是一棵二分搜索树2 TODO
     public boolean isBST2() {
         return isBST2(root, null, null);
     }
@@ -200,6 +203,57 @@ public class AVLTree<K extends Comparable<K>, V> {
         if (low != null && node.key.compareTo(low) <= 0) return false;
         if (high != null && node.key.compareTo(high) >= 0) return false;
 
-        return isBST2(node.left, null, node.key) && isBST2(node.right, node.key, null);
+        return isBST2(node.left, low, node.key) && isBST2(node.right, node.key, high);
+    }
+
+    // 判断以Node为根的二叉树是否是一棵平衡二叉树，递归算法
+    private boolean isBalanced(Node node) {
+        if (node == null)
+            return true;
+        int balanceFactor = getBalanceFactor(node);
+        if (balanceFactor > 1)
+            return false;
+        return isBalanced(node.left) && isBalanced(node.right);
+    }
+
+
+    // 对节点y进行向右旋转操作，返回旋转后新的根节点x
+    //        y                              x
+    //       / \                           /   \
+    //      x   T4     向右旋转 (y)        z     y
+    //     / \       - - - - - - - ->    / \   / \
+    //    z   T3                       T1  T2 T3 T4
+    //   / \
+    // T1   T2
+    private Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T3 = x.right;
+        x.right = y;
+        y.left = T3;
+
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+        return x;
+    }
+
+    // 对节点y进行向左旋转操作，返回旋转后新的根节点x
+    //    y                             x
+    //  /  \                          /   \
+    // T1   x      向左旋转 (y)       y     z
+    //     / \   - - - - - - - ->   / \   / \
+    //   T2  z                     T1 T2 T3 T4
+    //      / \
+    //     T3 T4
+    private Node leftRotate(Node y) {
+        Node x = y.right;
+        Node T2 = x.left;
+        x.left = y;
+        y.right = T2;
+
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+        return x;
     }
 }
